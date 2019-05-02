c6b95e1ead9d79ec9da89c2870eb3570dab38e84
Panzeraa <alexpanz06@gmail.com>
2019-05-02 17:27:52 +0200

    changement logo

--- logo.php
+++ logo.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>PF_CV</title>
+	<title>Panzera - Logo</title>
 	<link rel="stylesheet" href="styles.css">
 	<!-- <link rel="icon" type="image/png" href="assets/rdsurvivor_icone.png" /> -->
 
